e227ed8ba4e68037b1d97c78b58cc23ab7ae463b
akinabt <abekmambetova364@gmail.com>
2022-09-07 02:05:03 +0600

    file with our views-functions

--- meals_reciepts/reciepts/views.py
+++ meals_reciepts/reciepts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Menu
+from django.http import Http404
+import random
 # Create your views here.
+
+
+def main_page(request):
+    return HttpResponse('Привет, это сайт рецептов')
+
+
+def view_menu(request):
+    print(request)
+
+    my_object = Menu.objects.all()
+    context = {
+        "objects": my_object
+    }
+    return render(request, 'new-page/.html', context=context)
+
+
+
+
